a7040e5ed76f2dc11887a39fbf3db1354097fddb
thisis_elina <cc221001@fhstp.ac.at>
2023-06-23 18:08:12 +0200

    appvue documentation

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,17 +1,21 @@
 <script setup>
-import {onBeforeUpdate, onMounted, ref,} from "vue";
+import { onBeforeUpdate, onMounted, ref } from "vue";
 
-const loggedInUser = ref()
+// Importing necessary components
 import Footer from "./components/Footer.vue";
 import CoolHeader from "./components/CoolHeader.vue";
 
+// Declare reactive variables
+const loggedInUser = ref();
 
+// This hook is called when the component is mounted to the DOM
 onMounted(async () => {
   await login();
-})
+});
 
-
+// Function to perform login
 const login = async () => {
+  // Send a GET request to the login API endpoint
   let response = await fetch('http://localhost:3000/api/login', {
     method: 'GET',
     headers: {
@@ -24,20 +28,23 @@ const login = async () => {
   // Handle the response from the server
   const responseData = await response.json();
   if (responseData.success) {
-    loggedInUser.value = responseData.data
-    console.log(loggedInUser.value)
+    // Update the value of loggedInUser with the data from the response
+    loggedInUser.value = responseData.data;
+    console.log(loggedInUser.value);
   } else {
     // Handle any errors that occur during the request
     console.log(responseData.error);
   }
-}
+};
 </script>
 
 <template>
+  <!-- Main layout template -->
   <CoolHeader></CoolHeader>
   <router-view></router-view>
   <Footer></Footer>
 </template>
 
 <style scoped>
+/* Scoped CSS styles specific to this component */
 </style>
